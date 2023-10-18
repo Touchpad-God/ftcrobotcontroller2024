8eaee1382cae609cdfd2c1a6fc38f8c6df9efa1d
Touchpad-God <kevinchangyupeng@gmail.com>
2023-10-17 20:07:42 -0400

    paths for robot

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MyClass.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MyClass.java
@@ -3,6 +3,7 @@ package com.example.meepmeeptesting;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
@@ -18,23 +19,44 @@ public class MyClass {
         Runnable runnable = () -> {
 
         };
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity red = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+
+                // Get pose from AprilTag first, no implementation yet
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setColorScheme(new ColorSchemeBlueDark())
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(36, 12, Math.toRadians(180)))
+                                .addSpatialMarker(new Vector2d(-24, 56), () -> {
+                                    // Intake
+                                    // Use AprilTag to get pose
+                                })
+                                .setReversed(true)
+                                .splineTo(new Vector2d(54, 30), Math.toRadians(180))
+                                .splineToSplineHeading(new Pose2d(30, 46, Math.toRadians(90)), Math.toRadians(90))
+                                .splineToConstantHeading(new Vector2d(12, 36), Math.toRadians(90))
+                                .lineToConstantHeading(new Vector2d(12, -56))
+                                .lineToConstantHeading(new Vector2d(12, 36))
+                                .splineTo(new Vector2d(30, 48), Math.toRadians(90))
+                                .build()
+                );
+
+        RoadRunnerBotEntity blue = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
 
                 // Get pose from AprilTag first, no implementation yet
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(30, 48, Math.toRadians(90)))
-                                .addSpatialMarker(new Vector2d(24, -56), () -> {
+                        drive.trajectorySequenceBuilder(new Pose2d(-36, -36, Math.toRadians(0)))
+                                .addSpatialMarker(new Vector2d(24, 56), () -> {
                                     // Intake
                                     // Use AprilTag to get pose
                                 })
-                                .lineToSplineHeading(new Pose2d(36, 0, Math.toRadians(270)))
-                                .splineToConstantHeading(new Vector2d(36, -20), Math.toRadians(270))
-                                .splineToConstantHeading(new Vector2d(24, -56), Math.toRadians(270))
-                                .lineToSplineHeading(new Pose2d(12, -16, Math.toRadians(90)))
-                                .splineToConstantHeading(new Vector2d(12, 10), Math.toRadians(90))
-                                .splineToConstantHeading(new Vector2d(30, 48), Math.toRadians(90))
+                                .setReversed(true)
+                                .splineToConstantHeading(new Vector2d(-48, -48), Math.toRadians(-90))
+                                .splineToSplineHeading(new Pose2d(-36, -56, Math.toRadians(90)), Math.toRadians(0))
+                                .splineToConstantHeading(new Vector2d(-12, -48), Math.toRadians(90))
+                                .splineToConstantHeading(new Vector2d(-12, -24), Math.toRadians(90))
                                 .build()
                 );
 
@@ -48,7 +70,8 @@ public class MyClass {
         meepMeep.setBackground(img)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
+                .addEntity(red)
+                .addEntity(blue)
                 .start();
 
     }
