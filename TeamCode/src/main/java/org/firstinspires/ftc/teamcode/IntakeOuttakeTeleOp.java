f283d850d61306d87772b594e5a30f995514c0bc
Epicotch <xiujulv@gmail.com>
2023-12-24 11:44:59 -0500

    nuked unused imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeOuttakeTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeOuttakeTeleOp.java
@@ -1,17 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
-import android.graphics.Color;
-
 import com.acmerobotics.dashboard.config.Config;
-import com.qualcomm.hardware.rev.RevColorSensorV3;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
-
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 /*
 
