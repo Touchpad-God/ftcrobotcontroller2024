eb3a731deb9e7c29b8fb621d4db74531f8fc11fe
Touchpad-God <kevinchangyupeng@gmail.com>
2023-12-26 10:33:26 -0500

    working on auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterStageUpperAutoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterStageUpperAutoRed.java
@@ -26,100 +26,99 @@ public class CenterStageUpperAutoRed extends LinearOpMode{
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-
-        waitForStart();
-
         intakeOuttake = new IntakeOuttakeAuto(hardwareMap);
         Thread inOutThread = new Thread(intakeOuttake);
-        Thread time = new Thread(new Time());
-        time.start();
+//        Thread time = new Thread(new Time());
+//        time.start();
         inOutThread.start();
         intakeOuttake.transferState = IntakeOuttake.TransferState.MOTORS;
 
+        waitForStart();
+
         if (isStopRequested()) return;
 
-        drive.followTrajectory(drive.trajectoryBuilder(new Pose2d()).forward(-25).build());
-
-        drive.setPoseEstimate(new Pose2d(36, 12, Math.toRadians(0)));
-
-        location = 3;
-
-        if (location == 1) { // center
-            traj = drive.trajectorySequenceBuilder(new Pose2d(36, 12, Math.toRadians(0)))
-                    .setReversed(true)
-                    .addDisplacementMarker(() -> {
-                        intakeOuttake.outtakeState = IntakeOuttake.OuttakeState.AUTORAISED;
-                        while(intakeOuttake.outtakeState != IntakeOuttake.OuttakeState.POS4) {
-                            idle();
-                        }
-                    })
-                    .lineToSplineHeading(new Pose2d(36, 48, Math.toRadians(270)))
-                    .addDisplacementMarker(() -> {
-                        intakeOuttake.setCurrTime(getRuntime());
-                        intakeOuttake.outtakeState = IntakeOuttake.OuttakeState.DROPPED;
-                        while(intakeOuttake.outtakeState != IntakeOuttake.OuttakeState.IDLE) {
-                            idle();
-                        }
-                    });
-//                    .splineToConstantHeading(new Vector2d(12, 36), Math.toRadians(270))
-//                    .splineToConstantHeading(new Vector2d(12, -56), Math.toRadians(270))
-//                    .lineToConstantHeading(new Vector2d(12, 26))
-//                    .splineTo(new Vector2d(30, 48), Math.toRadians(90)
-
-        } else if (location == 2) { // left
-            traj = drive.trajectorySequenceBuilder(new Pose2d(36, 12, Math.toRadians(0)))
-                    .setReversed(true)
-                    .turn(Math.toRadians(90))
-                    .addDisplacementMarker(() -> {
-                        intakeOuttake.outtakeState = IntakeOuttake.OuttakeState.AUTORAISED;
-                        while(intakeOuttake.outtakeState != IntakeOuttake.OuttakeState.POS4) {
-                            idle();
-                        }
-                    })
-                    .lineToSplineHeading(new Pose2d(30, 48, Math.toRadians(270)))
-                    .addDisplacementMarker(() -> {
-                        intakeOuttake.setCurrTime(getRuntime());
-                        intakeOuttake.outtakeState = IntakeOuttake.OuttakeState.DROPPED;
-                        while(intakeOuttake.outtakeState != IntakeOuttake.OuttakeState.IDLE) {
-                            idle();
-                        }
-                    });
-        } else { //right
-            traj = drive.trajectorySequenceBuilder(new Pose2d(36, 12, Math.toRadians(0)))
-                    .setReversed(true)
-                    .turn(Math.toRadians(-90));
-            drive.followTrajectorySequence(traj.build());
-
-            intakeOuttake.outtakeState = IntakeOuttake.OuttakeState.AUTORAISED;
-            while(intakeOuttake.outtakeState != IntakeOuttake.OuttakeState.POS4) {
-                idle();
-            }
-
-            traj = drive.trajectorySequenceBuilder(new Pose2d(36, 12, Math.toRadians(-90)))
-                    .splineToConstantHeading(new Vector2d(48, 24), Math.toRadians(90))
-                    .splineToConstantHeading(new Vector2d(42, 48), Math.toRadians(180));
-
-            intakeOuttake.outtakeState = IntakeOuttake.OuttakeState.DROPPED;
-            while(intakeOuttake.outtakeState != IntakeOuttake.OuttakeState.IDLE) {
-                idle();
-            }
-
-//                    .splineToConstantHeading(new Vector2d(12, 36), Math.toRadians(270))
-//                    .splineToConstantHeading(new Vector2d(12, -56), Math.toRadians(270))
-//                    .lineToConstantHeading(new Vector2d(12, 26))
-//                    .splineTo(new Vector2d(30, 48), Math.toRadians(90)
-//            .addDisplacementMarker(() -> {
-//                intakeOuttake.setCurrTime(getRuntime());
-//                intakeOuttake.outtakeState = IntakeOuttake.OuttakeState.AUTORAISED;
-//                while(intakeOuttake.outtakeState != IntakeOuttake.OuttakeState.POS4) {
-//                    idle();
-//                }
+//        drive.followTrajectory(drive.trajectoryBuilder(new Pose2d()).forward(-24).build());
+//
+//        drive.setPoseEstimate(new Pose2d(37.5, 12, Math.toRadians(0)));
+//
+//        location = 3;
+//
+//        if (location == 1) { // center
+//            traj = drive.trajectorySequenceBuilder(new Pose2d(37.5, 12, Math.toRadians(0)))
+//                    .setReversed(true)
+//                    .addDisplacementMarker(() -> {
+//                        intakeOuttake.outtakeState = IntakeOuttake.OuttakeState.AUTORAISED;
+//                        while(intakeOuttake.outtakeState != IntakeOuttake.OuttakeState.POS4) {
+//                            idle();
+//                        }
+//                    })
+//                    .lineToSplineHeading(new Pose2d(36, 48, Math.toRadians(270)))
+//                    .addDisplacementMarker(() -> {
+//                        intakeOuttake.setCurrTime(getRuntime());
+//                        intakeOuttake.outtakeState = IntakeOuttake.OuttakeState.DROPPED;
+//                        while(intakeOuttake.outtakeState != IntakeOuttake.OuttakeState.IDLE) {
+//                            idle();
+//                        }
+//                    });
+////                    .splineToConstantHeading(new Vector2d(12, 36), Math.toRadians(270))
+////                    .splineToConstantHeading(new Vector2d(12, -56), Math.toRadians(270))
+////                    .lineToConstantHeading(new Vector2d(12, 26))
+////                    .splineTo(new Vector2d(30, 48), Math.toRadians(90)
+//
+//        } else if (location == 2) { // left
+//            traj = drive.trajectorySequenceBuilder(new Pose2d(37.5, 12, Math.toRadians(0)))
+//                    .setReversed(true)
+//                    .turn(Math.toRadians(90))
+//                    .addDisplacementMarker(() -> {
+//                        intakeOuttake.outtakeState = IntakeOuttake.OuttakeState.AUTORAISED;
+//                        while(intakeOuttake.outtakeState != IntakeOuttake.OuttakeState.POS4) {
+//                            idle();
+//                        }
+//                    })
+//                    .lineToSplineHeading(new Pose2d(30, 48, Math.toRadians(270)))
+//                    .addDisplacementMarker(() -> {
+//                        intakeOuttake.setCurrTime(getRuntime());
+//                        intakeOuttake.outtakeState = IntakeOuttake.OuttakeState.DROPPED;
+//                        while(intakeOuttake.outtakeState != IntakeOuttake.OuttakeState.IDLE) {
+//                            idle();
+//                        }
+//                    });
+//        } else { //right
+//            traj = drive.trajectorySequenceBuilder(new Pose2d(37.5, 12, Math.toRadians(0)))
+//                    .setReversed(true)
+//                    .turn(Math.toRadians(-90));
+//            drive.followTrajectorySequence(traj.build());
+//
+//            intakeOuttake.outtakeState = IntakeOuttake.OuttakeState.AUTORAISED;
+//            while(intakeOuttake.outtakeState != IntakeOuttake.OuttakeState.POS4) {
+//                idle();
 //            }
-
-        }
-
-        drive.followTrajectorySequence(traj.build());
-        intakeOuttake.stop();
+//
+//            traj = drive.trajectorySequenceBuilder(new Pose2d(36, 12, Math.toRadians(-90)))
+//                    .splineToConstantHeading(new Vector2d(48, 24), Math.toRadians(90))
+//                    .splineToConstantHeading(new Vector2d(42, 48), Math.toRadians(180));
+//
+//            intakeOuttake.outtakeState = IntakeOuttake.OuttakeState.DROPPED;
+//            while(intakeOuttake.outtakeState != IntakeOuttake.OuttakeState.IDLE) {
+//                idle();
+//            }
+//
+////                    .splineToConstantHeading(new Vector2d(12, 36), Math.toRadians(270))
+////                    .splineToConstantHeading(new Vector2d(12, -56), Math.toRadians(270))
+////                    .lineToConstantHeading(new Vector2d(12, 26))
+////                    .splineTo(new Vector2d(30, 48), Math.toRadians(90)
+////            .addDisplacementMarker(() -> {
+////                intakeOuttake.setCurrTime(getRuntime());
+////                intakeOuttake.outtakeState = IntakeOuttake.OuttakeState.AUTORAISED;
+////                while(intakeOuttake.outtakeState != IntakeOuttake.OuttakeState.POS4) {
+////                    idle();
+////                }
+////            }
+//
+//        }
+//
+//        drive.followTrajectorySequence(traj.build());
+//        intakeOuttake.stop();
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeOuttakeAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeOuttakeAuto.java
@@ -5,21 +5,23 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class IntakeOuttakeAuto extends IntakeOuttake implements Runnable {
-    private Telemetry telemetry;
     private boolean running;
+    public static double startTime;
     public static double currTime;
 
     public IntakeOuttakeAuto(HardwareMap hardwareMap) {
         super(hardwareMap);
+        startTime = (double) System.currentTimeMillis() / 1000;
         this.running = true;
     }
     @Override
     public void run() {
         while (running) {
-            telemetry.addData("Intake state", intakeState);
-            telemetry.addData("Outtake state", outtakeState);
-            telemetry.addData("Transfer state", transferState);
-            telemetry.addData("Outtake Ticks", outtakeTicks);
+            currTime = ((double) System.currentTimeMillis() / 1000) - startTime;
+//            telemetry.addData("Intake state", intakeState);
+//            telemetry.addData("Outtake state", outtakeState);
+//            telemetry.addData("Transfer state", transferState);
+//            telemetry.addData("Outtake Ticks", outtakeTicks);
             intake(currTime);
             transfer(currTime);
             outtake(currTime);
