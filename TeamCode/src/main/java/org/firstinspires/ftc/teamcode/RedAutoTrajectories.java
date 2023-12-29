ca24d1282bb877bb4f7053085c6f1c0cad0c0f2c
Touchpad-God <kevinchangyupeng@gmail.com>
2023-12-29 13:36:29 -0500

    save this for later to sub in for all the trajectories in red auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutoTrajectories.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceBuilder;
+
+public class RedAutoTrajectories {
+    TrajectorySequenceBuilder visionCenter;
+    TrajectorySequenceBuilder visionLeft;
+    TrajectorySequenceBuilder visionRight;
+    public RedAutoTrajectories() {
+
+    }
+}
